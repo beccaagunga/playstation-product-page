d119de8e0e791870877e8899aa523350105f0403
Becca Agunga <beccaagunga@gmail.com>
2020-01-03 17:36:14 -0500

    added color selections

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+
+
 window.onload = function () {
-// get arrow
-const color = document.getElementById('color');
+// get contorller color
+const black = document.getElementsByClassName('black')[0];
+const white = document.getElementsByClassName('white')[0];
+const purple = document.getElementsByClassName('purple')[0];
 // get controller
 const controller = document.getElementById('controller');
-// listen for click on arrow
-color.addEventListener('click', changeImg);
+// listen for click of each color
+black.addEventListener('click', changeImgBlack);
+white.addEventListener('click', changeImgWhite);
+purple.addEventListener('click', changeImgPurple);
 // function to change img
-function changeImg() {
+function changeImgBlack() {
+  controller.src = "img/blk-controller.png";
+  }
+function changeImgWhite() {
   controller.src = "img/white-controller.png";
-}
+  }
+function changeImgPurple() {
+  controller.src = "img/purple-controller.png";
+  }
+
 }
